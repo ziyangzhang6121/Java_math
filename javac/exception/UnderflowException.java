c8d67495b0d49e7528861bb96e3b7145772d15ea
ziyangzhang6121 <152249079+ziyangzhang6121@users.noreply.github.com>
2023-11-29 16:54:10 +0800

    Add files via upload

---
+++ javac/exception/UnderflowException.java
@@ -0,0 +1,8 @@
+package javac.exception;
+
+@SuppressWarnings("serial")
+public class UnderflowException extends Exception {
+	public UnderflowException() {
+		// TODO Auto-generated constructor stub
+	}
+}
